2bf3423128b0986d0f4ee41cb0edc09bc12fc165
Frank Merema <frank.merema@asml.com>
2019-03-05 15:09:43 +0100

    Added some more content

--- index.js
+++ index.js
@@ -7,46 +7,63 @@ const response = chalk.bold.green;
 
 const resume = require('./resume.json');
 
-const resumePrompts = {
-    type: 'list',
-    name: 'resumeOptions',
-    message: "What do you want to know about me?",
-    choices: [...Object.keys(resume), 'Exit']
-};
-
 const main = () => {
     console.log('Hello, my name is Frank Merema and welcome to my resume');
     resumeHandler();
 };
 
 const resumeHandler = () => {
+    const resumePrompts = {
+        type: 'list',
+        name: 'mainOptions',
+        message: "What do you want to know about me?",
+        choices: [...Object.keys(resume), 'Exit']
+    };
+
     inquirer.prompt(resumePrompts).then(answer => {
-        if (answer.resumeOptions === 'Exit') {
-            return;
+        switch (answer[resumePrompts.name]) {
+            case 'Exit':
+                break;
+            case 'Working Experience':
+                experienceHandler();
+                break;
+            default:
+                printData(resume[answer[resumePrompts.name]]);
+
+                resumeHandler();
+                break;
         }
+    });
+};
 
-        const option = answer.resumeOptions;
+const experienceHandler = () => {
+    const experiencePrompt = {
+        type: 'list',
+        name: 'experienceOptions',
+        message: "About which assignment do you want to see more info?",
+        choices: [...resume['Working Experience'], 'Back']
+    };
 
-        console.log(response("--------------------------------------"));
+    inquirer.prompt(experiencePrompt).then(answer => {
+        switch (answer[experiencePrompt.name]) {
+            case 'Back':
+                resumeHandler();
+                break;
+            default:
+                printData([answer[experiencePrompt.name]]);
+                experienceHandler();
+                break;
+        }
+    });
+};
 
-        resume[`${option}`].forEach(info => {
-            console.log(response("| " + info));
-        });
-
-        console.log(response("--------------------------------------"));
-
-        inquirer
-            .prompt({
-                type: "list",
-                name: "exitBack",
-                message: "Go back or Exit?",
-                choices: ["Back", "Exit"]
-            })
-            .then(choice => {
-                if (choice.exitBack === 'Back') {
-                    resumeHandler();
-                }
-            });
+const printData = (data) => {
+    data.forEach(info => {
+        if (Array.isArray(info)) {
+            printData(info)
+        } else {
+            console.log(response(info));
+        }
     });
 };
 
